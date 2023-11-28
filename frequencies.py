7de1f29acfdb766d93cb8fd8f695aa6ac32f762f
Brian <probrian89@gmail.com>
2023-11-29 02:18:14 +0800

    freq

--- frequencies.py
+++ frequencies.py
@@ -2,15 +2,15 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
-     if not items:
-      return{}
-     frequencies = {}
-     for item in items:
-       item_str = str(item)  
-     if item_str in frequencies:
-       frequencies[item_str] += 1
-     else:
-       frequencies[item_str] = 1
+ if not items:
+   return{}
+ frequencies = {}
+ for item in items:
+  item_str = str(item)  
+  if item_str in frequencies:
+     frequencies[item_str] += 1
+  else:
+     frequencies[item_str] = 1
       
-     return frequencies
+ return frequencies
 
